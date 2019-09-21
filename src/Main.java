595e6597a4b0188cc9a790ea4eaa318f1a0c3913
Manuel Werder <manuel.werder@csbe.ch>
2019-09-21 17:29:32 +0200

    Start of documentation of the project

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import controller.DungeonController;
 
+
+/**
+ * The entry point to the Dungeon game.
+ *
+ * @author Manuel Werder
+ * @version 0.1
+ */
 public final class Main {
 
     public static void main(String[] args) {
-//        System.out.println("Hello, world!");
 
         DungeonController controller = new DungeonController(args);
-
 
     }
 
--- src/controller/DungeonController.java
+++ src/controller/DungeonController.java
@@ -2,10 +2,30 @@ package controller;
 
 import userInterface.UserCLIInterface;
 
+
+/**
+ * The DungeonController describes a way to play multiple text based Dungeon-Crawler-Games.
+ * It can be given as an argument over the CommandLine while starting the game or when the game is
+ * started, where the player may chose from a list of games.
+ *
+ * @author Manuel Werder
+ * @version 0.1
+ */
 public class DungeonController {
 
     private UserCLIInterface userInterface;
 
+    /**
+     * The Constructor for the DungeonController will call internally a private method,
+     * witch will evaluate if there is a valid argument and set up the game, or will
+     * show a list of games to play.
+     *
+     * When given an argument (one argument only) it will work
+     * or it will tell the user that there is no game with the given name
+     * and quit the application.
+     *
+     * @param args Takes one argument. It must be a name to a possible game.
+     */
     public DungeonController(String[] args) {
         this.userInterface = new UserCLIInterface();
 
@@ -13,4 +33,6 @@ public class DungeonController {
         String userInput = userInterface.getInput("dungeon@menace> ");
         userInterface.print(userInput, "\n");
     }
+
+
 }
