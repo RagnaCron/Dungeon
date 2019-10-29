4e8be144ed77a91a5c24c8504487aa358a86d1a4
manuel.werder <manuel.werder@csbe.ch>
2019-10-29 12:27:11 +0100

    generator

---
+++ src/main/java/Generator/DungeonGenerator.java
@@ -0,0 +1,12 @@
+package Generator;
+
+/**
+ * The DungeonGenerator class is a Singleton that is capable
+ * to generate random dungeons maps with monster and loot.
+ *
+ * @author Manuel Werder
+ * @version 0.1
+ */
+public class DungeonGenerator {
+
+}
